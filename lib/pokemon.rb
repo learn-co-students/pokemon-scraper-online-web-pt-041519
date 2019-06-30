5be4586b1fb024681d23c5677d957d6b67e8eb32
earthctzn <earthctzn1@gmail.com>
2019-06-29 23:23:55 -0400

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -14,7 +14,14 @@ class Pokemon
         db.execute("INSERT INTO pokemon (name, type) VALUES (?, ?)", name, type)
     end
 
+    def self.find(id, db)
+        sql = <<-SQL
+        SELECT * FROM pokemon WHERE id = ?
+        SQL
+        arr = db.execute(sql, id).first
+        Pokemon.new(id: arr[0], name: arr[1], type: arr[2], db: db)
 
+    end
 
 
 
