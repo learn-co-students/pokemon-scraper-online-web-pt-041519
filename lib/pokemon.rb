89944b7eb33718904245ce49367e7e71b7bec637
earthctzn <earthctzn1@gmail.com>
2019-06-30 00:54:51 -0400

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -19,15 +19,33 @@ class Pokemon
         SELECT * FROM pokemon WHERE id = ?
         SQL
         arr = db.execute(sql, id).first
-        Pokemon.new(id: arr[0], name: arr[1], type: arr[2], db: db)
+        Pokemon.new(id: id, name: arr[1], type: arr[2], db: db)
 
     end
 
 
 
+    # def alter_hp(health, db)
+    #     sql = <<-SQL 
+    #     UPDATE pokemon SET name = ?, 
+    #     type = ?, 
+    #     hp = ? WHERE id = ? 
+    #     SQL
+    #     db.execute(sql, self.name, self.type, health, self.id)
+    # end
 
+    # def alter_hp(health, db)
+    #     sql = <<-SQL
+    #       UPDATE pokemon 
+    #       SET hp = ?
+    #       WHERE id = ?
+    #     SQL
+    #     db.execute(sql, health, @id)
+    #   end
 
-
+    # def alter_hp(health, db)
+    #     db.execute("UPDATE pokemon SET hp = ? WHERE id = ?", health, self.id)
+    # end 
 
 
 end
