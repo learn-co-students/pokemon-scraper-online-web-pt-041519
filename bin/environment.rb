2bd0449923fffb29aa1b5f57d97273e9c4ffe4e1
earthctzn <earthctzn1@gmail.com>
2019-06-29 22:52:38 -0400

    Done.

--- bin/environment.rb
+++ bin/environment.rb
@@ -8,7 +8,7 @@ require_relative "../lib/pokemon"
 require_relative "../lib/scraper"
 require_relative "sql_runner"
 
-@db = SQLite3::Database.new('../pokemon.db')
-@db.execute("DROP TABLE IF EXISTS pokemon;")
-@sql_runner = SQLRunner.new(@db)
+DB = {:conn => SQLite3::Database.new('../pokemon.db')}
+DB[:conn].execute("DROP TABLE IF EXISTS pokemon;")
+@sql_runner = SQLRunner.new(DB[:conn])
 @sql_runner.execute_schema_migration_sql
--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,31 @@
 class Pokemon
+    attr_accessor :name, :type, :db
+    attr_reader :id
+
+    def initialize(id:, name:, type:, db:)
+        @id = id
+        @name = name
+        @type = type
+        @db = db
+    end
+
+    def self.save(name, type, db)
+        sql = <<-SQL
+        INSERT INTO pokemon (name, type, db)
+        VALUES (?, ?, ?)
+        SQL
+        #binding.pry
+        DB[:conn].execute(sql)
+        @id = DB[:conn].execute("SELECT last_insert_rowid() FROM pokemon")[0][0]
+    end
+
+
+
+
+
+
+
+
+
+
 end
