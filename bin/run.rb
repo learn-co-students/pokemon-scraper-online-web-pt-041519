3d49d8d6a89ceb4dc47b48dfefe44e62772545e4
Iskra B <ibatistapoblete@gmail.com>
2019-07-04 09:03:03 +0000

    Done.

--- bin/run.rb
+++ bin/run.rb
@@ -5,3 +5,8 @@ Scraper.new(@db).scrape
 all_pokemon = @db.execute("SELECT * FROM pokemon;")
 
 # test out your code here!
+
+db = SQLite3::Database.new('../db/pokemon.db')
+sql_runner = SQLRunner.new(db)
+
+sql_runner.execute_sql_file
