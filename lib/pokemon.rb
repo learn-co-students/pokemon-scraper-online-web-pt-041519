f94f5eb6368dda3b4e7425cde638efa5d97af7c2
Junyoung McArdle <juny.mcardle@gmail.com>
2019-07-08 04:04:33 +0000

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,17 +1,44 @@
 class Pokemon
 
-  attr_accessor :name, :type, :db, :hp
-  attr_reader :id
+  attr_accessor :id, :name, :type, :db, :hp
 
-  def initialize(id=nil, name, type, dp)
+  def initialize(keywords)
     @id = id
     @name = name
     @type = type
-    @dp = dp
+    @db = db
+    @hp = hp
   end
 
-  def self.save
-    db.execute("INSERT INTO pokemon (name, type) VALUES (?, ?)")
+  def self.save(name, type, db)
+    sql = <<-SQL
+      INSERT INTO pokemon (name, type) VALUES (?, ?)
+    SQL
+
+    db.execute(sql, name, type)
   end
+
+  def self.find(num, db)
+    sql = <<-SQL
+      SELECT * FROM pokemon WHERE id = ?
+    SQL
+    pokemon = db.execute(sql, num).first
+    new_pokemon = Pokemon.new(pokemon)
+
+    new_pokemon.id = pokemon[0]
+    new_pokemon.name = pokemon[1]
+    new_pokemon.type = pokemon[2]
+
+    return new_pokemon
+  end
+
+  # def alter_hp(new_hp, db)
+  #   sql = <<-SQL
+  #     UPDATE pokemon SET hp = ? WHERE id = ?
+  #   SQL
+  #
+  #   db.execute(sql, new_hp, self.id).first
+  #   self.hp = new_hp
+  # end
 
 end
